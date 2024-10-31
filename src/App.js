5b8077b59620debebc8d04bba2ea67294d8831fc
sam__charles__ <sc240843@gmail.com>
2024-10-31 10:44:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ import Certificates from './components/Certificates';
 import Contact from './components/Contact';
 
 function App() {
+
   return (
     <div className="App container">
       <header>
         <h1>Welcome to <span className='green'>Sam__Charles__ </span> Portfolio</h1>
         <p>B.E Computer Science Engineering (CSE) Final Year Student @ Parisutham Institute of Technology and Science (2021 - 2025) Batch, Thanjavur.</p>
+        <a href="https://forms.gle/GNYnzzy8Zts81Lug6">
+          <button type="button" class="btn btn-primary position-relative"> Feedback <span class="position-absolute top-0 start-100 translate-middle badge  p-2"><span class="visually-hidden">unread messages</span></span>
+          </button>
+        </a>
       </header>
       <AboutMe/>
       <Education />
